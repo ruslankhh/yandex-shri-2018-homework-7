1e6333e27586169023d42287ac2d1be18114bf0b
Ruslan Khusnetdinov <ruslankhh@gmail.com>
2018-04-06 00:10:57 +0300

    feat: Обновил запуск аудита

--- bin/update_report_desktop.sh
+++ bin/update_report_desktop.sh
@@ -2,11 +2,10 @@
 
 export DATE=`date '+%Y-%m-%d_%H-%M-%S'`
 
-mkdir ./reports/$DATE-desktop
 ed -s ./reports/index.html <<- EOM
 /<!-- END REPORTS LIST -->/i
       <li>
-        <a href="$DATE-desktop/$DATE-desktop-lifehacker.ru.html">
+        <a href="$DATE-desktop-lifehacker.ru.html">
           $DATE-desktop-lifehacker.ru
         </a>
       </li>
--- bin/update_report_mobile.sh
+++ bin/update_report_mobile.sh
@@ -2,11 +2,10 @@
 
 export DATE=`date '+%Y-%m-%d_%H-%M-%S'`
 
-mkdir ./reports/$DATE-mobile
 ed -s ./reports/index.html <<- EOM
 /<!-- END REPORTS LIST -->/i
       <li>
-        <a href="$DATE-mobile/$DATE-mobile-lifehacker.ru.html">
+        <a href="$DATE-mobile-lifehacker.ru.html">
           $DATE-mobile-lifehacker.ru
         </a>
       </li>
