45ece9781068143d661be514508d092521e306c3
Ruslan Khusnetdinov <ruslankhh@gmail.com>
2018-04-05 23:57:34 +0300

    feat: Обновил запуск аудита

--- bin/update_report_desktop.sh
+++ bin/update_report_desktop.sh
@@ -3,13 +3,13 @@
 export DATE=`date '+%Y-%m-%d_%H-%M-%S'`
 
 mkdir ./reports/$DATE-desktop
-ed -s reports/index.html <<- EOM
-/<\/ul>/i
-    <li>
-      <a href="./$DATE-desktop/lifehacker.ru-desktop_$DATE.html">
-        lifehacker.ru-desktop-$DATE
-      </a>
-    </li>
+ed -s ./reports/index.html <<- EOM
+/<!-- END REPORTS LIST -->/i
+      <li>
+        <a href="$DATE-desktop/$DATE-desktop-lifehacker.ru.html">
+          $DATE-desktop-lifehacker.ru
+        </a>
+      </li>
 .
 wq
 EOM
--- bin/update_report_mobile.sh
+++ bin/update_report_mobile.sh
@@ -3,13 +3,13 @@
 export DATE=`date '+%Y-%m-%d_%H-%M-%S'`
 
 mkdir ./reports/$DATE-mobile
-ed -s reports/index.html <<- EOM
-/<\/ul>/i
-    <li>
-      <a href="./$DATE-mobile/lifehacker.ru-mobile_$DATE.html">
-        lifehacker.ru-mobile-$DATE
-      </a>
-    </li>
+ed -s ./reports/index.html <<- EOM
+/<!-- END REPORTS LIST -->/i
+      <li>
+        <a href="$DATE-mobile/$DATE-mobile-lifehacker.ru.html">
+          $DATE-mobile-lifehacker.ru
+        </a>
+      </li>
 .
 wq
 EOM
