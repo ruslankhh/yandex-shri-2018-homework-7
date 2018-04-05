de7c685fc03a0ef103d6a4a3fec7b07a73e0cb49
Ruslan Khusnetdinov <ruslankhh@gmail.com>
2018-04-06 00:46:00 +0300

    feat: Добавил тест на psi

---
+++ bin/run_psi.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo '> psi https://lifehacker.ru --strategy=mobile'
+psi https://lifehacker.ru --strategy=mobile
+
+echo ''
+echo '> psi https://lifehacker.ru --strategy=desktop'
+psi https://lifehacker.ru --strategy=desktop
